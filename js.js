519276e19f228bedfb18d4738e03228aaef73fc1
Mark1906 <marksakharch@gmail.com>
2025-07-25 20:06:50 +0300

    js.js

--- js.js
+++ js.js
@@ -1,26 +1,27 @@
-let productsGrid = document.getElementById('products-grid');
-let productsArray =[];
-let xhr = new XMLHttpRequest();
+let productsGrid=document.getElementById('products-grid');
+let productsArray=[];
+let xhr =new XMLHttpRequest();
 let url = 'https://my-json-server.typicode.com/Mark1906/second-new';
 
+
 xhr.open('GET',url + '/products');
-xhr.responseType = 'json'
-xhr.onload = function() {
-       let poducts = xhr.response;
-       productsGrid.innerHTML = null;
-       productsArray.forEach(p => {
-           productsArray.push(p);
-           let pElem = document.createElement('div');
-           pElem.classList.add('product');
-           pElem.innerHTML =`
-              <h2 class='product-name'>${p.name}</h2>
-              <img class='product-photo' src='${p.photo-url}' alt='${p.name}'>
-              <p class='product-price'><b>Price: </b>${p.price}$</p>
-              <p class='product-description'><b>Description: </b>${p.description}</p>
-              <a href=">Seller profile</a>
-              <button>Buy</button>
-           `;  
-           productsGrid.append(pElem); 
-       });
+xhr.responseType='json'
+xhr.onload=function(){
+    let products=xhr.response;
+    productsGrid.innerHTML=null;
+    products.forEach(p=>{
+        productsArray.push(p);
+        let pElem=document.createElement('div');
+        pElem.classList.add('product'
+        );
+        pElem.innerHTML=`
+        <h2 class='product-name'>${p.name}</h2>
+        <img class='product-photo' src='${p.photo_url}' alt ='${p.name}'>
+        <p class='product-price><b>Price: </b>${p.prise}$</p>
+        <p class='product-description><b>Description: </b>${p.description}$</p>
+        <a href='userProfile.html?id=${p.author_id}'>Seller profile</a>
+        <button>Buy</button>`;
+        productsGrid.append(pElem);
+        });
 }
 xhr.send();
